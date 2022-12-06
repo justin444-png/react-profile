f0b1c4886440fefc3dcdd6f7a0c02ffc46d2793a
justin444-png <119054787+justin444-png@users.noreply.github.com>
2022-12-06 18:25:27 +0530

    2 files modified

--- Sidenav.tsx
+++ Sidenav.tsx
@@ -2,14 +2,40 @@ import * as React from 'react';
 import './style.css';
 
 export default function SideNav({ expandFlag }) {
+  let employees = [
+    {
+      id: 1,
+      name: "Tommy",
+      age: 23,
+      city: "New York",
+    },
+  
+    {
+      id: 2,
+      name: "Mike",
+      age: 27,
+      city: "Detroit",
+    },
+  
+    {
+      id: 3,
+      name: "Lisa",
+      age: 25,
+      city: "Chicago",
+    },
+  ];
   return (
     <div
-      className={ expandFlag ? 'mobilehight sideNavhed' : 'sideNavhed'}
+      className={expandFlag ? 'mobilehight sideNavhed' : 'sideNavhed'}
       style={{
         width: expandFlag ? '30%' : '5%',
       }}
     >
-      SideNavjkhkfhjkfhdfbdbdshjb
+      <div>
+      {employees.map((user) => (
+        <div className="list_name">{user.name}</div>
+      ))}
+    </div>
     </div>
   );
 }
